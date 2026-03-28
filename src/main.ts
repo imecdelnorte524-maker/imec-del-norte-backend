a758e6d5f6cbf6b1fc98a2bd3136b27ad70e8c78
Imec Del Norte <imec.del.norte524@gmail.com>
2026-03-28 07:53:06 -0500

    change domains access

--- src/main.ts
+++ src/main.ts
@@ -44,7 +44,7 @@ async function bootstrap() {
   const allowedOrigins = [
     'https://imec-del-norte-web.up.railway.app',
     'https://imec-del-norte-staggin.up.railway.app',
-    'imec-del-norte-backend.up.railway.app',
+    'https://imec-del-norte-backend.up.railway.app',
     'https://m3h6rtnz-3032.use.devtunnels.ms',
     'http://localhost:3032',
     'http://localhost:3000',
